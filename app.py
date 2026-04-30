7d64e7f97622eb5bc9fd9ae9e1a2c437a5fff4d7
Afifatul Rohmah <affifatulrohmah@gmail.com>
2026-04-30 13:16:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 # Database configuration
 db_config = {
-    'host': os.getenv('DB_HOST', 'secure-inventory-stack-mydbinstance-fjjzarrvk0xe.ciu1ki93xdcq.us-east-1.rds.amazonaws.com'),
+    'host': os.getenv('DB_HOST', 'secure-inventory-stack-mydbinstance-q7xeusipujxm.ciu1ki93xdcq.us-east-1.rds.amazonaws.com'),
     'user': os.getenv('DB_USER', 'admin'),
     'password': os.getenv('DB_PASS', 'Password123'),
     'database': os.getenv('DB_NAME', 'guestbook_db')
